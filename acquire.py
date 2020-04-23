0a0b047fb2c1941698f8b33abc0f644fc998e147
Alec Hartman <aleclhartman08@gmail.com>
2020-04-23 13:41:48 -0500

    better query

--- acquire.py
+++ acquire.py
@@ -8,7 +8,7 @@ def get_db_url(dbname) -> str:
     return url.format(user, password, host, dbname)
 
 telco_query = """
-SELECT c.*, i.internet_service_type, ct.contract_type, p.payment_type
+SELECT c.customer_id, c.gender, c.senior_citizen, c.partner, c.dependents, c.tenure, c.phone_service, c.multiple_lines, i.internet_service_type, c.online_security, c.online_backup, c.device_protection, c.tech_support, c.streaming_tv, c.streaming_movies, ct.contract_type, c.paperless_billing, p.payment_type, c.monthly_charges, c.churn
 FROM customers AS c
 JOIN internet_service_types AS i ON c.internet_service_type_id = i.internet_service_type_id
 JOIN contract_types AS ct ON c.contract_type_id = ct.contract_type_id
