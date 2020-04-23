7c185c424ab96a2e460db2a6f51e075b19fcc490
Alec Hartman <aleclhartman08@gmail.com>
2020-04-23 16:11:33 -0500

    added total charges to query

--- acquire.py
+++ acquire.py
@@ -8,7 +8,7 @@ def get_db_url(dbname) -> str:
     return url.format(user, password, host, dbname)
 
 telco_query = """
-SELECT c.customer_id, c.gender, c.senior_citizen, c.partner, c.dependents, c.tenure, c.phone_service, c.multiple_lines, c.internet_service_type_id, i.internet_service_type, c.online_security, c.online_backup, c.device_protection, c.tech_support, c.streaming_tv, c.streaming_movies, c.contract_type_id, ct.contract_type, c.paperless_billing, c.payment_type_id, p.payment_type, c.monthly_charges, c.churn
+SELECT c.customer_id, c.gender, c.senior_citizen, c.partner, c.dependents, c.tenure, c.phone_service, c.multiple_lines, c.internet_service_type_id, i.internet_service_type, c.online_security, c.online_backup, c.device_protection, c.tech_support, c.streaming_tv, c.streaming_movies, c.contract_type_id, ct.contract_type, c.paperless_billing, c.payment_type_id, p.payment_type, c.monthly_charges, c.total_charges, c.churn
 FROM customers AS c
 JOIN internet_service_types AS i ON c.internet_service_type_id = i.internet_service_type_id
 JOIN contract_types AS ct ON c.contract_type_id = ct.contract_type_id
